c237e1c3a42d57af1cc37feda9006ead9037cdc6
Alicia Fasciocco <afasciocco@gmail.com>
2019-07-27 21:08:48 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -16,7 +16,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    until input = "exit"
+    until input == "exit"
       input = gets
     if input == "list songs"
       list_songs
