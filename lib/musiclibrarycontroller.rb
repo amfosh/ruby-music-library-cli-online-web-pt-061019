fe068c9bdfba8ce4d58aeb13a62ae35f55e11221
Alicia Fasciocco <afasciocco@gmail.com>
2019-07-27 21:12:34 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -34,7 +34,7 @@ class MusicLibraryController
   end
 end
 
-  def list_songs 
+  def list_songs
     Song.all.sort_by{|x| x.name}.each.with_index(1) do |song, index|
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
